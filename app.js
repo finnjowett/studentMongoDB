f38a7719e538d1c4bd515b6501c2e0a16d32344f
finn <finnjowett971108@gmail.com>
2022-11-21 12:31:24 -0800

    final

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost:27017/studentDB");
+const methodOverride = require("method-override");
 const Student = require("./models/student");
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use(methodOverride("_method"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
@@ -42,14 +44,33 @@ app.post("/students/insert", (req, res) => {
       res.render("reject.ejs");
     });
 });
+
+// show mongodb data on the webpage
 app.get("/students/:id", async (req, res) => {
   let { id } = req.params;
   // findone return an object, find return an array
   let data = await Student.findOne({ id });
   if (data !== null) {
-    res.render("studentPage", { data });
+    res.render("studentPage.ejs", { data });
   } else {
     res.send("sorry, cannot complete your request");
   }
+});
+
+app.get("/students/edit:id", async (req, res) => {
+  let { id } = req.params;
+  try {
+    let data = await Student.findOne({ id });
+    if (data !== null) {
+      res.render("edit", { data });
+    } else {
+      res.send("cannot find this student");
+    }
+  } catch (e) {
+    res.send(e.message);
+  }
+});
+app.put("/students/edit:id", (req, res) => {
+  console.log(req.body);
 });
 app.listen(3000);
